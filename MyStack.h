86496b1ca97e1cfb0caf99d12f9e67621fd77e20
Anton Goy <g.a.s.s@mail.ru>
2014-04-11 12:41:45 +0600

    First classes!

--- MyStack.h
+++ MyStack.h
@@ -4,3 +4,15 @@
  * @author Anton Goy <antongoy92@mail.ru>
 */
 
+/**
+ * MyStack class represents my implementation of the famous data structure Stack.
+*/
+
+template <class T> class MyList {
+	T List_Item_Value;
+	MyList <T>  * Next_Item;
+}
+
+template <class T> class MyStack {
+	
+}
